8d34c22413a7c8169ea9e249a4b9fdc021153da1
piroman <marcin.babiuch92@gmail.com>
2021-11-10 00:14:54 +0100

    Done def __init__

--- prob_calculator.py
+++ prob_calculator.py
@@ -4,5 +4,13 @@ import random
 
 class Hat:
 
+    def __init__(self, **kwargs):
+        contents = []
+        for arg in kwargs:
+            for ball in range(kwargs[arg]):
+                contents.append(arg)
+        print(contents)
+
+
 
 # def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
