98e2c69b78cd8a14a610382b5c9df0b4e0ae6b70
piroman <marcin.babiuch92@gmail.com>
2021-11-20 23:09:05 +0100

    Final version

--- prob_calculator.py
+++ prob_calculator.py
@@ -15,12 +15,24 @@ class Hat:
         if num_draw > len(self.contents):
             return self.contents
 
-        for choosen_ball_index in range(num_draw):
-            choosen_ball_index = random.randrange(0, len(self.contents))
-            choosen_balls.append(self.contents[choosen_ball_index])
-            self.contents.pop(choosen_ball_index)
+        for i in range(num_draw):
+            random_ball = random.randint(0, len(self.contents) - 1)
+            choosen_balls.append(self.contents[random_ball])
+            self.contents.remove(self.contents[random_ball])
         return choosen_balls
 
+def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+    M = 0
 
+    for numbers in range(num_experiments):
+        expected_copy = copy.deepcopy(expected_balls)
+        hat_copy = copy.deepcopy(hat)
+        colors_gotten = hat_copy.draw(num_balls_drawn)
+        list_of_trues = []
+        for key in expected_copy:
+            if colors_gotten.count(key) >= expected_copy[key]:
+                list_of_trues.append(True)
+                if len(list_of_trues) == len(expected_balls):
+                    M += 1
 
-# def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+    return M/num_experiments
